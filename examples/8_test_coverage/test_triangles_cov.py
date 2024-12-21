12c0bacaf2e59d9ed4d357f39003552eaa672a0b
Chris Mayfield <mayfiecs@jmu.edu>
2024-12-21 13:39:05 -0700

    working version of test coverage; not quite done

--- examples/8_test_coverage/test_triangles_cov.py
+++ examples/8_test_coverage/test_triangles_cov.py
@@ -3,6 +3,7 @@
 from jmu_pytest_utils.common import assert_pep8
 from jmu_pytest_utils.coverage import assert_fail, assert_pass, assert_cover
 from jmu_pytest_utils.decorators import required, weight
+import pytest
 
 SUBMISSION_FILES = ["test_triangles.py"]
 ADDITIONAL_FILES = ["triangles.py"]
@@ -17,17 +18,26 @@ def test_pep8():
 @weight(2)
 def test_fail():
     """All tests should fail when given random return values"""
-    assert_fail("triangles.py", "test_triangles.py")
+    count, output = assert_fail("triangles.py", "test_triangles.py")
+    if count:
+        test_fail.score = max(test_fail.weight - count, 0)
+        pytest.fail(output)
 
 
 @required()
 @weight(2)
 def test_pass():
-    """All tests should pass when given correct return values"""
-    assert_pass("triangles.py", "test_triangles.py")
+    """All tests should pass when given actual return values"""
+    count, output = assert_pass("triangles.py", "test_triangles.py")
+    if count:
+        test_pass.score = max(test_pass.weight - count, 0)
+        pytest.fail(output)
 
 
 @weight(5)
 def test_cover():
     """Code coverage: all statements should run during tests"""
-    assert_cover("triangles.py", "test_triangles.py")
+    count, output = assert_cover("triangles.py", "test_triangles.py", True)
+    if count:
+        test_cover.score = max(test_cover.weight - count, 0)
+        pytest.fail(output)
--- jmu_pytest_utils/coverage.py
+++ jmu_pytest_utils/coverage.py
@@ -1,9 +1,11 @@
 """Run test coverage and analyze the results."""
 
 import importlib
+import json
 import os
 import pytest
 import sys
+import textwrap
 import types
 
 from jmu_pytest_utils.common import run_command
@@ -29,24 +31,57 @@ def inject_random(main_filename):
             obj.__code__ = return_random.__code__
 
 
+def process_results_json(status):
+    """Verify correctness in results.json file.
+
+    Args:
+        status (str): Check for "fail" or "pass" in status.
+
+    Returns:
+        int, str: Number of errors and corresponding output.
+    """
+    if not os.path.exists("results.json"):
+        pytest.fail("pytest failed to generate test results", False)
+    with open("results.json") as file:
+        results = json.load(file)
+    os.remove("results.json")
+
+    # Count how many test functions didn't pass/fail correctly
+    count = 0
+    if status == "fail":
+        output = "random return values\n"
+    else:
+        output = "actual return values\n"
+    for test in results["tests"]:
+        if test["status"] != status + "ed":  # failed / passed
+            count += 1
+            output += f"* {test['name']} did not {status}\n"
+            test_output = test.get("output")
+            if test_output:
+                output += textwrap.indent(test_output, "    ")
+                output += "\n"
+    return count, output
+
+
 def assert_fail(main_filename, test_filename):
     """Run pytest and assert that all tests fail.
 
-    This test patches all functions to return random numbers.
+    Note: The --jmu option of the jmu_pytest_utils plugin
+    patches all functions in main_filename to return random.
 
     Args:
         main_filename (str): Name of the main file to test.
         test_filename (str): Name of the test file to run.
+
+    Returns:
+        int, str: Number of errors and corresponding output.
     """
     run_command([
         "pytest",
         "--jmu=" + main_filename,
         test_filename
     ])
-    if not os.path.exists("results.json"):
-        pytest.fail("pytest failed to generate test results", False)
-    pass  # TODO
-    os.remove("results.json")
+    return process_results_json("fail")
 
 
 def assert_pass(main_filename, test_filename):
@@ -55,33 +90,58 @@ def assert_pass(main_filename, test_filename):
     Args:
         main_filename (str): Name of the main file to test.
         test_filename (str): Name of the test file to run.
+
+    Returns:
+        int, str: Number of errors and corresponding output.
     """
     run_command([
         "pytest",
         "--jmu=assert_pass",
         test_filename
     ])
-    if not os.path.exists("results.json"):
-        pytest.fail("pytest failed to generate test results", False)
-    pass  # TODO
-    os.remove("results.json")
+    return process_results_json("pass")
 
 
-def assert_cover(main_filename, test_filename):
+def assert_cover(main_filename, test_filename, branches=False):
     """Run pytest and analyze coverage results.
 
     Args:
         main_filename (str): Name of the main file to test.
         test_filename (str): Name of the test file to run.
+
+    Returns:
+        int, str: Number of missed and corresponding output.
     """
     run_command([
         "pytest",
         "--cov=" + main_filename[:-3],  # remove .py suffix
-        "--cov-branch",
+        "--cov-branch" if branches else "",
         "--cov-report=json",
         test_filename
     ])
     if not os.path.exists("coverage.json"):
         pytest.fail("pytest failed to generate coverage results", False)
-    pass  # TODO
+    with open("coverage.json") as file:
+        coverage = json.load(file)
     os.remove("coverage.json")
+
+    # Count how many lines were missed in each function
+    count = 0
+    output = "incomplete coverage\n"
+    functions = coverage["files"][main_filename]["functions"]
+    for name, data in functions.items():
+        missing_lines = data.get("missing_lines", [])
+        missing_branches = data.get("missing_branches", [])
+        missed = len(missing_lines) + len(missing_branches)
+        if missed:
+            count += missed
+            output += f"* {name}\n"
+            if missing_lines:
+                output += "    Lines: "
+                output += str(missing_lines)[1:-1]
+                output += "\n"
+            if missing_branches:
+                output += "    Branches: "
+                output += str(missing_branches)[1:-1]
+                output += "\n"
+    return count, output
