c93f24113768797c3a8a7aacacd0fe0b235335c1
Chris Mayfield <mayfiecs@jmu.edu>
2025-10-30 17:36:08 -0400

    Bump version to 1.6.0

--- jmu_pytest_utils/template/setup.sh
+++ jmu_pytest_utils/template/setup.sh
@@ -16,7 +16,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 
 # Install libraries
-pip install git+https://github.com/JMU-CS/jmu_pytest_utils.git@v1.5.0
+pip install git+https://github.com/JMU-CS/jmu_pytest_utils.git@v1.6.0
 if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
 fi
